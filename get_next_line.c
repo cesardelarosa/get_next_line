24c28aa69274626e2529612a518562a3d4cf48d7
Cesar De La Rosa Sobrino <cde-la-r@c1r11s5.42madrid.com>
2023-10-18 18:30:12 +0200

    simplified (1)

--- get_next_line.c
+++ get_next_line.c
@@ -6,66 +6,36 @@
 /*   By: cde-la-r <cde-la-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 04:50:34 by cde-la-r          #+#    #+#             */
-/*   Updated: 2023/10/18 13:07:32 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:28:17 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*read_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*jump;
 	static char	buffer[BUFFER_SIZE + 1];
-	char	 	*r;
-	
+	char		*r;
+
 	r = (char *)malloc(sizeof(char));
 	*r = '\0';
-	if (!jump)
-		jump = buffer;
-	if (jump != buffer)
+	if (jump && jump < &buffer[BUFFER_SIZE + 1])
 	{
 		r = ft_strjoin(r, ++jump);
-		if ((jump = ft_strchr(jump, '\n')) != NULL)
+		jump = ft_strchr(jump, '\n');
+		if (jump)
 			return (r);
 	}
 	while (1)
 	{
 		if (read(fd, buffer, BUFFER_SIZE) < 1)
-			break;
+			break ;
 		r = ft_strjoin(r, buffer);
-		if ((jump = ft_strchr(buffer, '\n')) != NULL)
-			break;
+		jump = ft_strchr(buffer, '\n');
+		if (jump)
+			break ;
 	}
+	r = ft_strndup(r, ft_strchr(r, '\n') - r + 1);
 	return (r);
-}
-
-char	*cut_line(char *str)
-{
-	char	*r;
-	
-	r = ft_strndup(str, ft_strchr(str, '\n') - str + 1);
-	return (r);
-}
-
-char	*get_next_line(int fd)
-{
-	char		*r;
-
-	r = read_line(fd);
-	r = cut_line(r);
-	return (r);
-}
-
-int		main(void)
-{
-	int	fd;
-
-	fd = open("a.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	close (fd);
-	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <cde-la-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 04:50:42 by cde-la-r          #+#    #+#             */
-/*   Updated: 2023/10/18 12:56:03 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2023/10/18 17:38:28 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ char	*ft_strndup(const char *s, size_t n);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dest, const char *src, size_t n);
 char	*ft_strjoin(const char *s1, const char *s2);
-char	*read_line(int fd);
 char	*get_next_line(int fd);
 
 #endif
