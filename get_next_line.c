49b01bb8ca64ac9b9c581b521bd6af2b0db73586
Cesar De La Rosa Sobrino <cde-la-r@c2r14s1.42madrid.com>
2023-10-21 21:13:28 +0200

    bad day

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <cde-la-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 04:50:34 by cde-la-r          #+#    #+#             */
-/*   Updated: 2023/10/18 20:00:21 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2023/10/21 21:10:24 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,34 @@ char	*get_next_line(int fd)
 	char		*r;
 
 	r = NULL;
-	if (jump && jump < &buffer[BUFFER_SIZE + 1])
+	if (jump && *jump)
 	{
-		r = ft_strjoin(r, ++jump);
+		r = ft_strjoin(r, jump);
+		if (!r)
+		{
+			free(r);
+			return (NULL);
+		}
 		jump = ft_strchr(jump, '\n');
 		if (jump)
 			return (r);
 	}
-	while (1)
+	jump = NULL;
+	while (read(fd, buffer, BUFFER_SIZE) > 0 && !jump)
 	{
-		if (read(fd, buffer, BUFFER_SIZE) < 1)
-			break ;
 		r = ft_strjoin(r, buffer);
+		if (!r)
+		{
+			free (r);
+			return (NULL);
+		}
 		jump = ft_strchr(buffer, '\n');
-		if (jump)
-			break ;
 	}
 	r = ft_strndup(r, ft_strchr(r, '\n') - r + 1);
+	if (!r)
+	{
+		free (r);
+		return (NULL);
+	}
 	return (r);
 }
